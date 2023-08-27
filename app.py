a1d723e779a4f9c62ae3016b3a45a652a2925102
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-08-28 00:51:03 +0530

    new file:   app.py 	new file:   examples/2582289.jpg 	new file:   examples/3622237.jpg 	new file:   examples/592799.jpg 	new file:   food.pth 	new file:   model.py 	new file:   requirements.txt

---
+++ app.py
@@ -0,0 +1,93 @@
+### 1. Imports and class names setup ###
+
+import gradio as gr
+import os
+import torch
+
+from model import create_effnetb2_model
+from timeit import default_timer as timer
+from typing import Tuple, Dict
+
+# Setup class names
+
+class_names = ['pizza', 'steak', 'sushi']
+
+### 2. Model and transforms perparation ###
+
+effnetb2, effnetb2_transforms = create_effnetb2_model(
+    num_classes=3)
+
+# Load save weights
+
+effnetb2.load_state_dict(
+    torch.load(
+        f="food.pth",
+        map_location=torch.device("cpu")  # load the model to the CPU
+    )
+)
+
+### 3. Predict function ###
+
+
+def predict(img) -> Tuple[Dict, float]:
+    
+    # Start a timer
+
+    start_time = timer()
+
+    # Transform the input image for use with EffNetB2
+    # unsqueeze = add batch dimension on 0th index
+
+    img = effnetb2_transforms(img).unsqueeze(0)
+
+    # Put model into eval mode, make prediction
+
+    effnetb2.eval()
+    with torch.inference_mode():
+        
+        # Pass transformed image through the model and turn the prediction logits into probaiblities
+
+        pred_probs = torch.softmax(effnetb2(img), dim=1)
+
+    # Create a prediction label and prediction probability dictionary
+
+    pred_labels_and_probs = {class_names[i]: float(
+        pred_probs[0][i]) for i in range(len(class_names))}
+
+    # Calculate pred time
+
+    end_time = timer()
+    pred_time = round(end_time - start_time, 4)
+
+    # Return pred dict and pred time
+
+    return pred_labels_and_probs, pred_time
+
+
+### 4. Gradio app ###
+
+
+# Create title, description and article
+
+title = "FoodVision Mini 🍕🥩🍣"
+description = "An [EfficientNetB2 feature extractor](https://pytorch.org/vision/stable/models/generated/torchvision.models.efficientnet_b2.html#torchvision.models.efficientnet_b2) computer vision model to classify images as pizza, steak or sushi."
+article = "Food Vision"
+
+# Create example list
+
+example_list = [["examples/" + example] for example in os.listdir("examples")]
+
+# Create the Gradio demo
+
+demo = gr.Interface(fn=predict,  # maps inputs to outputs
+                    inputs=gr.Image(type="pil"),
+                    outputs=[gr.Label(num_top_classes=3, label="Predictions"),
+                             gr.Number(label="Prediction time (s)")],
+                    examples=example_list,
+                    title=title,
+                    description=description,
+                    article=article)
+
+# Launch the demo!
+
+demo.launch()
